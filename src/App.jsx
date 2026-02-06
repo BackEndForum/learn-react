8a01f2b4bd8adfd0384cf0b660353fd5d1bde3b6
Back.end.forum <back.end.forum.php@gmail.com>
2026-02-06 10:26:47 +0400

    Destructuring in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,52 @@
 
 import './App.css'
-import data from "./assets/MOCK_DATA.json";
-const langs = ["html","css","js","react"];
-const list = data;
+const lang = ["html", "css", "js", "php"];
 
-const fruitlist = ['apple', 'banana', 'cherry'];
+const [one, , thre, last] = lang;
 
+const handel = function(confDate) {
+  const d = confDate.getDate();
+  const m = confDate.getMonth() + 1;
+  const y = confDate.getFullYear();
+  return [d, m, y];
+}
+
+const [day, month, year] = handel(new Date());
+
+// const objTest = {name: "BMW", model: "2023", color: "white"};
+
+const objTest = function(obj) {
+  const x = obj.name;
+  const y = obj.model;
+  const z = obj.color;
+  return {x, y, z}
+}
+const {y, z, x} = objTest({name: "BMW", model: "2024", color: "white"});
+
+const testObj = objTest({name:"BYD", model: "2022", color: "black"});
 
 function App() {
 
-  return (
-    <div className='App'>
-      <h2>welcome mustafa</h2>
-      <ul>
-        {langs.map(lang => <li key={lang}>{lang}</li>)}
-      </ul>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>first name</th>
-            <th>gender</th>
-            <th>e-mail</th>
-            <th>phone</th>
-          </tr>
-        </thead>
-        <tbody>
-
-          {list.map(val => <tr key={val.id}><td >{val.id}</td><td>{val.first_name + " " + val.last_name}</td><td>{val.gender}</td><td>{val.email}</td><td>{val.phone}</td></tr>)}
-        </tbody>
-      </table>
-      <ul>
-      {fruitlist.map((fruit, index, array) => {
-        return (
-          <li key={fruit}>
-            Number: {fruit}, Index: {index}, Array: {array}
-          </li>
-        );
-      })}
+  return <div>
+    <p>the first lang is {one}</p>
+    <p>the second lang is </p>
+    <p>the three lang is {thre}</p>
+    <p>and the last lang is {last}</p>
+    <br />
+    <mark>today is {month} {day}, {year}</mark>
+    <br />
+    <ul>
+      <li>the name is {testObj.x}</li>
+      <li>the model is {testObj.y}</li>
+      <li>the color is {testObj.z}</li>
     </ul>
-    </div>
-  )
+    <br />
+    <ul>
+      <li>the name is {x}</li>
+      <li>the model is {y}</li>
+      <li>the color is {z}</li>
+    </ul>
+  </div>
 }
 
 export default App
